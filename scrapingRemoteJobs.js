f17908ae51505b4dc345c8b1a7ec07d0c80fabda
DavidPalomeque <davidpalomeque2001@gmail.com>
2020-01-31 21:44:17 -0300

    json for google sheets added

--- scrapingRemoteJobs.js
+++ scrapingRemoteJobs.js
@@ -13,19 +13,26 @@ const fs = require("fs");
 
         var content = await page.evaluate(() => {
             var links = document.querySelectorAll(".elementor-text-editor ul li a[href]")
-            var linksArray = []
+            var jsonContent = {
+                websites : [] , 
+                urls : []
+            }
+            // var linksArray = []
             for (let i = 0; i < links.length; i++) {
                 var linksText = links[i].innerText.trim()
                 var linksUrl = links[i].getAttribute("href")
-                linksArray.push(linksText + " : " + linksUrl + "\n")
+                jsonContent.websites.push(linksText)
+                jsonContent.urls.push(linksUrl)
+                // linksArray.push(linksText + " : " + linksUrl + "\n")
             }
-            return linksArray.join("")
+            return jsonContent
+            // return linksArray.join("")
         })
 
         await browser.close(); // close browser
-        fs.writeFile("remoteJobs.txt" , content , function (err) {
+        fs.writeFile("remoteJobs.json" , JSON.stringify(content) , function (err) {
             if (err) throw err
-            console.log("Done !");
+            console.log("Json Saved !");
         })
 
     } catch (err) {
