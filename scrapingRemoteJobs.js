cab5fb87f18afe760ce1ed6c2f610c8c20804298
DavidPalomeque <davidpalomeque2001@gmail.com>
2020-02-01 01:40:51 -0300

    scrapped : txt & json generated

--- scrapingRemoteJobs.js
+++ scrapingRemoteJobs.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-// node remoteJobs.js
+// node scrapingRemoteJobs.js
 ( async () => {
 
     try {
@@ -11,33 +11,41 @@ const fs = require("fs");
         await page.goto("https://yourdreamremotejobnow.com/2020-long-list-of-remote-job-boards-for-any-professionals-in-any-industry-based-in-any-location/?fbclid=IwAR3hlX9nacKKb3BD5u7XIiKidiS-Y7PrbpgeyFUIGB7T5bLVAkd-4cjbhJE" , {waitUntil: 'load', timeout: 0}); // url
         await page.waitForSelector("#main"); // selector
 
-        var content = await page.evaluate(() => {
-            var links = document.querySelectorAll(".elementor-text-editor ul li a[href]")
+        var content = await page.evaluate(() => { // main function
+            var links = document.querySelectorAll(".elementor-text-editor ul li a[href]") // selected element/s
+            // variables , objects , arrays to return
+            var linksArray = []
+            var linksTextArray = []
+            var linksUrlArray = []
             var jsonContent = {
                 websites : [] , 
                 urls : []
             }
-            // var linksArray = []
-            for (let i = 0; i < links.length; i++) {
+            for (let i = 0; i < links.length; i++) { // for each element selected
                 var linksText = links[i].innerText.trim()
                 var linksUrl = links[i].getAttribute("href")
                 jsonContent.websites.push(linksText)
                 jsonContent.urls.push(linksUrl)
-                // linksArray.push(linksText + " : " + linksUrl + "\n")
+                linksArray.push(linksText + " : " + linksUrl + "\n")
+                linksTextArray.push(linksText + "\n")
+                linksUrlArray.push(linksUrl + "\n")
             }
-            return jsonContent
+            // then return ONLY ONE of these
+            // return jsonContent
             // return linksArray.join("")
+            // return linksTextArray.join("")
+            // return linksUrlArray.join("")
         })
 
         await browser.close(); // close browser
-        fs.writeFile("remoteJobs.json" , JSON.stringify(content) , function (err) {
+        fs.writeFile("remoteJobs.txt" , /* JSON.stringify(content) */ content , function (err) { // write txt / json file
             if (err) throw err
-            console.log("Json Saved !");
+            console.log("Done !");
         })
 
-    } catch (err) {
+    } catch (err) { // if there is some error
 
-        console.log(err);
+        console.log(err); // show me the error
         await browser.close();
         console.log("Browser Closed...")
 
